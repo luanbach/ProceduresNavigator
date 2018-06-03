5153e0876850a9bf4cb24917d63212f99a2ef6ac
Luan Bach <luan.bach@touchsurgery.com>
2018-06-03 12:03:05 +0100

    Initial Commit

---
+++ ProceduresNavigatorTests/ProceduresNavigatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProceduresNavigatorTests.swift
+//  ProceduresNavigatorTests
+//
+//  Created by Luan Bach on 03/06/2018.
+//  Copyright © 2018 Little Blue Dot. All rights reserved.
+//
+
+import XCTest
+@testable import ProceduresNavigator
+
+class ProceduresNavigatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
